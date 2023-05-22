13a3a1a6a8b90aa7ec4ef4996233955b8bbe23c1
daniel shinoda pascoal <danielshinodapascoal@daniels-MacBook-Pro.local>
2023-05-22 14:32:11 -0300

    archinsole

--- models/staging/ga4/base/base_ga4__events.sql
+++ models/staging/ga4/base/base_ga4__events.sql
@@ -64,7 +64,7 @@ with source as (
         select * from {{ source('ga4_356935471', 'events_intraday') }}
         where cast( _table_suffix as int64) >= 20230101
         union all
-        select * from {{ source('ga4_3375649600', 'events_intraday') }}
+        select * from {{ source('ga4_375649600', 'events_intraday') }}
         where cast( _table_suffix as int64) >= 20230101
 
 
@@ -84,7 +84,7 @@ with source as (
         where _table_suffix not like '%intraday%'
         and cast( _table_suffix as int64) >= 20230101
         union all
-        select *,_table_suffix as TABLE_SUFFIX_2 from {{ source('ga4_3375649600', 'events') }}
+        select *,_table_suffix as TABLE_SUFFIX_2 from {{ source('ga4_375649600', 'events') }}
         where _table_suffix not like '%intraday%'
         and cast( _table_suffix as int64) >= 20230101
         )
